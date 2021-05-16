6908547740572a1ba55362e6bd12b827d8da1a53
amrdovic <72739061+amrdovic1@users.noreply.github.com>
2021-05-16 18:07:02 +0200

    Zapoceta implementacija, provjera je li sve ok

---
+++ Implementacija/RahatBookApp/Models/Rezervacija.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Class1
+{
+	static int brojRezervacija;
+	int id;
+	int idKorisnika;
+	int idSJ;
+	Tuple(DateTime, DateTime) dolazakOdlazak;
+	public Class1()
+	{
+	}
+}
