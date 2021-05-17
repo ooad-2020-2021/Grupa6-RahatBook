a41f92f28abb853e23fb64b63c1662f13c3a66aa
amrdovic <72739061+amrdovic1@users.noreply.github.com>
2021-05-17 13:29:09 +0200

    Dodana model klasa Rezervacija

---
+++ Aplikacija/RahatBookAplikacija/RahatBookAplikacija/RahatBookAplikacija/Models/Rezervacija.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RahatBookAplikacija.Models
+{
+    public class Rezervacija
+    {
+        static int brojRezervacija;
+        int id;
+        int idKorisnika;
+        int idSJ;
+        Tuple<DateTime, DateTime> dolazakOdlazak;
+        bool ekskurzija;
+    }
+}
