3ebc3c481c35ce44627c38712dddc89d4e97b791
amrdovic <72739061+amrdovic1@users.noreply.github.com>
2021-06-06 12:57:18 +0200

    Zapoceta migracija u bazu

--- Aplikacija/RahatBookAplikacija/RahatBookAplikacija/RahatBookAplikacija/Data/RahatBookContext.cs
+++ Aplikacija/RahatBookAplikacija/RahatBookAplikacija/RahatBookAplikacija/Data/RahatBookContext.cs
@@ -14,14 +14,40 @@ namespace RahatBookAplikacija.Data
         {
         }
 
-        public DbSet<RahatBookAplikacija.Models.SmjestajnaJedinica> SmjestajnaJedinica { get; set; }
+        public DbSet<SmjestajnaJedinica> SmjestajnaJedinica { get; set; }
+        public DbSet<Rezervacija> Rezervacija { get; set; }
+        public DbSet<RegistrovaniKorisnik> RegistrovaniKorisnik { get; set; }
+        public DbSet<Administrator> Administrator { get; set; }
+        public DbSet<Login> Login { get; set; }
 
-        public DbSet<RahatBookAplikacija.Models.Rezervacija> Rezervacija { get; set; }
+        public DbSet<Lokacija> Lokacija { get; set; }
 
-        public DbSet<RahatBookAplikacija.Models.RegistrovaniKorisnik> RegistrovaniKorisnik { get; set; }
+        public DbSet<Popust> Popust { get; set; }
 
-        public DbSet<RahatBookAplikacija.Models.Administrator> Administrator { get; set; }
+        public DbSet<Uplata> Uplata { get; set; }
 
-        public DbSet<RahatBookAplikacija.Models.Login> Login { get; set; }
+        public DbSet<Gost> Gost { get; set; }
+
+        public DbSet<KontaktInfo> KontaktInfo { get; set; }
+
+        public DbSet<Recenzija> Recenzija { get; set; }
+
+        public DbSet<Upit> Upit { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<SmjestajnaJedinica>().ToTable("SmjestajnaJedinica");
+            mb.Entity<Rezervacija>().ToTable("Rezervacija");
+            mb.Entity<RegistrovaniKorisnik>().ToTable("RegistrovaniKorisnik");
+            mb.Entity<Login>().ToTable("Login");
+            mb.Entity<Administrator>().ToTable("Administrator");
+            mb.Entity<Lokacija>().ToTable("Lokacija");
+            mb.Entity<Popust>().ToTable("Popust");
+            mb.Entity<Uplata>().ToTable("Uplata");
+            mb.Entity<Gost>().ToTable("Gost");
+            mb.Entity<KontaktInfo>().ToTable("KontaktInfo");
+            mb.Entity<Recenzija>().ToTable("Recenzija");
+            mb.Entity<Upit>().ToTable("Upit");
+        }
     }
 }
