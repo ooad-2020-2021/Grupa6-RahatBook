02f0ad7f4c11dece5182049a7b10c19f2c2ffbc1
amrdovic <72739061+amrdovic1@users.noreply.github.com>
2021-06-12 23:09:18 +0200

    Dodani stereotipi za autorizaciju

--- RahatBookApp/Data/Migrations/20210610084816_Migration2.Designer.cs
+++ RahatBookApp/Data/Migrations/20210610084816_Migration2.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RahatBookApp.Data;
 
 namespace RahatBookApp.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210610084816_Migration2")]
+    partial class Migration2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
